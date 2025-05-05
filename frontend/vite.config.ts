15d34b9b67f0078bf5e963a554b1dfe7c31ee9ce
Vinod Godti <vinod@frostinteractive.com>
2025-05-05 19:16:04 +0530

    Dockerize the frontend app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,10 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    //@ts-expect-error
     tailwindcss()
   ],
 })
